7cb9bf56366557a1e7687e873848749ac787cc94
Sumeet330 <sumeet330@twc.com>
2025-03-09 08:25:32 +0000

    added files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".service-card").forEach(card => {
+        card.addEventListener("click", () => {
+            alert("More details coming soon!");
+        });
+    });
+});
